9f2a1f5a2f568978da5fd9ad2694c075ac9c283f
Ruxaxup <cavernasboy@gmail.com>
2015-02-06 11:03:55 -0600

    Conexion Ethernet test 1

--- PruebaBarMicLuz/PruebaBarMicLuz.ino
+++ PruebaBarMicLuz/PruebaBarMicLuz.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <TSL2561.h> //Luminosidad
 #include <Adafruit_MPL115A2.h> //Barometro
+#include <Ethernet.h>
 
 Adafruit_MPL115A2 barometro; //Barometro
 TSL2561 luminosidad(TSL2561_ADDR_FLOAT); //Luminosidad
@@ -10,7 +11,62 @@ int ledMic = 7;
 int ledLuz = 12;
 int ledBar = 13;
 
+/***** Ethernet ******/
+#define thingName = "Test_Galileo_1"
+//MAC esta escrita en la etiqueta del puerto Ethernet
+byte mac[] = { 0x98, 0x4F, 0xEE, 0x00, 0xE5, 0x86 };
+EthernetClient client;
+char server[] = "www.dweet.io"; 
+/********************/
+
+boolean initializeEthernet(){
+  
+  //Start Ethernet connection
+  if(Ethernet.begin(mac) == 0){
+    Serial.print("Failed to configure Ethernet using DHCP");
+    return false;
+  }
+  Serial.print("Connecting..."); 
+  return true;
+}
+
+/** Recopila y manda las lecturas a Dweet **/
+void sendDweet(float presion, float temperatura, float ruido
+               uint32_t lumens)
+{
+
+  if(client.connect(server,80)){
+    //Estructura de los datos
+    client.print("GET /dweet/for/");
+    client.print(thingNam);
+    cllient.print("?pressure=");
+    client.print(presion)
+    client.print("&temp=");
+    client.print(temperatura);
+    client.print("&noise=");
+    client.print(ruido);
+    client.print("&lumens=");
+    client.print(lumens);
+    client.println(" HTTP/1.1");
+    
+    //Indicador de host y cierre de la conexion
+    client.println("Host: dweet.io");
+    client.println("Connection: close");
+    client.println();
+    client.flush();  
+  }else{
+    Serial.println("Connection to Dweet failed");
+  }
+}
+
 void setup() {
+  
+  //Ethernet initialization
+  if(!initializeEthernet()){
+    //No hacemos nada
+    while(true){}
+  }
+  
   // put your setup code here, to run once:
   Serial.begin(9600);
   barometro.begin();
@@ -83,6 +139,8 @@ void loop() {
   Serial.println("");
   delay(1000);
   digitalWrite(ledMic, LOW);
-
+  
+  //Empujar los datos a Dweet
+  sendDweet(presion, temperatura, ruido, luminosidadCompleta);
   
 }
