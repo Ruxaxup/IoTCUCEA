49a3a3664a7722321d0e2b963ba9ccda069769f5
Ruxaxup <cavernasboy@gmail.com>
2015-03-12 10:58:41 -0600

    HTTP Dweet

--- PruebaBarMicLuz/PruebaBarMicLuz.ino
+++ PruebaBarMicLuz/PruebaBarMicLuz.ino
@@ -2,6 +2,8 @@
 #include <TSL2561.h> //Luminosidad
 #include <Adafruit_MPL115A2.h> //Barometro
 #include <Ethernet.h>
+#define DEBUG 0
+
 
 Adafruit_MPL115A2 barometro; //Barometro
 TSL2561 luminosidad(TSL2561_ADDR_FLOAT); //Luminosidad
@@ -10,31 +12,60 @@ unsigned int sample;
 int ledMic = 7;
 int ledLuz = 12;
 int ledBar = 13;
-
+int ledError = 8;
 /***** Ethernet ******/
-#define thingName "Test_Galileo_1"
+#define thingName "Test-Galileo-1"
+#define GALILEO 1
+#define MAC_SIZE 6
 //MAC esta escrita en la etiqueta del puerto Ethernet
 byte mac[] = { 0x98, 0x4F, 0xEE, 0x00, 0xE5, 0x86 };
+byte mac1F7R[] = { 0x98, 0x4F, 0xEE, 0x00, 0xE5, 0xFB };
+byte mac1DN7[] = { 0x98, 0x4F, 0xEE, 0x00, 0xE1, 0xA6 };
+byte actualMac[6];
 EthernetClient client;
 char server[] = "www.dweet.io"; 
 /********************/
 
+void copyArray(byte* array_original, byte* arrayTo, int n){
+  int i = 0;
+  for(; i<n; i++){
+    arrayTo[i] = array_original[i];
+  }
+}
+
 boolean initializeEthernet(){
+  switch(GALILEO){
+    case 0:
+      copyArray(actualMac,mac,MAC_SIZE);
+    break;
+    case 1:
+      copyArray(actualMac,mac1F7R,MAC_SIZE);
+    break;
+    case 2:
+      copyArray(actualMac,mac1DN7,MAC_SIZE);
+    break;
+  }
+  //habilitamos el puerto ethernet de galileo
+    if(DEBUG == 1) Serial.print("Setting up ethernet port");
+  system("ifup eth0");
+  delay(2000); 
+    if(DEBUG == 1) Serial.print("done");
   
   //Start Ethernet connection
   if(Ethernet.begin(mac) == 0){
-    Serial.print("Failed to configure Ethernet using DHCP");
+      if(DEBUG == 1) Serial.print("Failed to configure Ethernet using DHCP");
     return false;
   }
-  Serial.print("Connecting..."); 
+    if(DEBUG == 1) Serial.print("Connecting..."); 
   return true;
 }
 
 /** Recopila y manda las lecturas a Dweet **/
-void sendDweet(float presion, float temperatura, float ruido,
+void sendDweet(float presion, float temperatura, double ruido,
                uint32_t lumens)
 {
-
+  ruido = 20 * log10(ruido / 5);
+    if(DEBUG == 1) Serial.println("Dweeting...");
   if(client.connect(server,80)){
     //Estructura de los datos
     client.print("GET /dweet/for/");
@@ -54,69 +85,60 @@ void sendDweet(float presion, float temperatura, float ruido,
     client.println("Connection: close");
     client.println();
     client.flush();  
-  }else{
-    Serial.println("Connection to Dweet failed");
-  }
+    
+    //Leemos la respuesta
+    if(client.available()){
+      char c = client.read();
+      Serial.print(c);
+    }
+    client.stop();
+      if(DEBUG == 1) Serial.println("Dweet sent");
+    }else{
+        if(DEBUG == 1) Serial.println("Connection to Dweet failed");
+    }
 }
 
-void setup() {
-  
-  //Ethernet initialization
-  if(!initializeEthernet()){
-    //No hacemos nada
-    while(true){}
-  }
-  
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  barometro.begin();
-  pinMode(ledMic, OUTPUT);
-  digitalWrite(ledMic,LOW);
-  pinMode(ledLuz, OUTPUT);
-  digitalWrite(ledLuz,LOW);
-  pinMode(ledBar, OUTPUT);    
-  digitalWrite(ledBar,LOW);
+float getPressure(){
+  float presion = barometro.getPressure();
+  return presion;
 }
 
-void loop() {
-  //Variables para presion y temperatura
-  float presion = 0, temperatura = 0;
-  //Variables para luminosidad
-  uint32_t luminosidadCompleta;
-  //Variables microfono
+float getTemperature(){
+  float temperature = barometro.getTemperature();
+  return temperature;
+}
 
-  unsigned int peekToPeek = 0;
-  unsigned int signalMax = 0, signalMin = 1024;
-  
-  //Lectura de presion y temperatura
-  digitalWrite(ledBar, HIGH);
-  presion = barometro.getPressure();
-  temperatura = barometro.getTemperature();
+void printToSerialTempPress(float temp, float pressure){
   //Mostramos datos leidos
   Serial.print("Presion (kPa): ");
-  Serial.print(presion, 4);
+  Serial.print(pressure, 4);
   Serial.println(" kPa");
   Serial.print("Temperatura (*C): ");
-  Serial.print(temperatura, 1);
+  Serial.print(temp, 1);
   Serial.println(" *C");
-  Serial.println("/*************************/");  
-  delay(1000);
-  digitalWrite(ledBar, LOW);
-  //Lectura de luz
-  digitalWrite(ledLuz,HIGH);
-  luminosidadCompleta = luminosidad.getFullLuminosity();
+  Serial.println("/*************************/"); 
+}
+
+uint32_t getLuminosity(){
+  uint32_t luminosidadCompleta = luminosidad.getFullLuminosity();
   luminosidadCompleta = luminosidadCompleta & 0xFFFF;
+  return luminosidadCompleta;
+}
+
+void printToSerialLum(uint32_t lum){
   //Mostramos la luminosidad completa
   Serial.print("Full: ");
-  Serial.print(luminosidadCompleta);
+  Serial.print(lum);
   Serial.println(" lumenes");
   Serial.println("/*************************/");
-  delay(1000);
-  digitalWrite(ledLuz,LOW);
+}
 
-  //Lectura de ruido
+float getNoise(){
+  unsigned int peekToPeek = 0;
+  unsigned int signalMax = 0, signalMin = 1024;
   unsigned long startMillis = millis();
   digitalWrite(ledMic, HIGH);
+  
   while(millis() - startMillis < sampleWindow){
     sample = analogRead(0);
     if(sample < 1024){
@@ -127,20 +149,86 @@ void loop() {
       }
     }
   } 
-  peekToPeek = signalMax - signalMin;
-  double volts = (peekToPeek * 3.3) / 1024;
-  
-  //Mostramos el ruido o lo que sea
-  //Serial.print(peekToPeek);
+  peekToPeek = signalMax - signalMin;  
+  //double ruido = (peekToPeek * 3.3) / 1024;
+  double ruido = 20 * log10(sample / 2);
+  return ruido;
+}
+
+void printToSerialNoise(double noise){
+   //Serial.print(peekToPeek);
   Serial.print("Ruido: ");
-  Serial.print(volts);
-  Serial.println(" V");
+  Serial.print(noise);
+  Serial.println(" dB");
   Serial.println("/--------------------/");
   Serial.println("");
+}
+
+
+void setup() {
+  if(DEBUG == 1) Serial.begin(9600);
+  //Ethernet initialization
+  if(!initializeEthernet()){
+    //No hacemos nada
+    while(true){
+        digitalWrite(ledError, HIGH);
+    }
+  }  
+  // put your setup code here, to run once:
+  barometro.begin();
+  pinMode(ledMic, OUTPUT);
+  digitalWrite(ledMic,LOW);
+  pinMode(ledLuz, OUTPUT);
+  digitalWrite(ledLuz,LOW);
+  pinMode(ledBar, OUTPUT);    
+  digitalWrite(ledBar,LOW);
+  pinMode(ledError, OUTPUT);
+  digitalWrite(ledError, LOW);
+}
+
+void loop() {
+  //Variables para presion y temperatura
+  float presion = 0, temperatura = 0;
+  //Variables para luminosidad
+  uint32_t luminosidadCompleta = -1;
+  //Variables microfono
+  double ruido = 0;
+  
+  
+  //Lectura de presion y temperatura
+  digitalWrite(ledBar, HIGH);
+  presion = getPressure();
+  if(presion == 0){
+    //error de lectura Presion
+  }
+  temperatura = getTemperature();
+  if(temperatura == 0){
+    //error de lectura temperatura
+  }  
+  if(DEBUG == 1) printToSerialTempPress(temperatura, presion);   
+  delay(1000);
+  digitalWrite(ledBar, LOW);
+  /******************************************************/
+  //Lectura de luz
+  digitalWrite(ledLuz,HIGH);
+  luminosidadCompleta = getLuminosity();
+  if(luminosidadCompleta == -1){
+    //error de lectura Luz
+  }
+  if(DEBUG == 1) printToSerialLum(luminosidadCompleta);
+  delay(1000);
+  digitalWrite(ledLuz,LOW);
+  /******************************************************/
+  //Lectura de ruido
+  ruido = getNoise();
+  if(ruido == 0){
+    //error de lectura Ruido
+  }
+  if(DEBUG == 1) printToSerialNoise(ruido);
   delay(1000);
   digitalWrite(ledMic, LOW);
   
   //Empujar los datos a Dweet
-  sendDweet(presion, temperatura, volts, luminosidadCompleta);
+  sendDweet(presion, temperatura, ruido, luminosidadCompleta);
   
 }
